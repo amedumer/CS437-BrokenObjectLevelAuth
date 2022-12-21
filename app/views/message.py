0a91a17f329ecbf7c63f83cdbddaab230772a28e
Dora Akbulut <akbulutdora@gmail.com>
2022-12-21 22:49:36 +0300

    add authorization

--- app/views/message.py
+++ app/views/message.py
@@ -1,6 +1,9 @@
 # Flask modules
 from flask_jwt_extended import jwt_required
 
+# Flask module for authorization check
+from flask_jwt_extended import get_jwt_identity
+
 # App modules
 from app import app
 from app.models.message import Message
@@ -8,8 +11,13 @@ from app.models.message import Message
 @app.route('/api/messages/<int:user_id>/<int:message_id>', methods=['GET'])
 @jwt_required()
 def get_message(user_id, message_id):
-    # claims = get_jwt()
-    # VULNERABILITY: No check on the user ID, allowing any user to retrieve any message
+    # Check for authorization
+
+    # ======== PROTECTION 1: AUTHORIZATION CHECK ========
+    if get_jwt_identity() != user_id:
+        return {"error": "Not Authorized."}, 401
+    # ======== PROTECTION 1: AUTHORIZATION CHECK ========
+    
     message = Message.query.get(message_id)
     if message == None:
         return {"error": "Not Found"}, 404
