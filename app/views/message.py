108d26e97160adb1c1fb7f82b1bf8c96558287a9
Yasin Ughur <yasinugur.cs@gmail.com>
2022-12-30 16:31:43 +0300

    remove comment part

--- app/views/message.py
+++ app/views/message.py
@@ -11,13 +11,6 @@ from app.models.message import Message
 @app.route('/api/messages/<int:user_id>/<int:message_id>', methods=['GET'])
 @jwt_required()
 def get_message(user_id, message_id):
-    # Check for authorization
-
-    # ======== PROTECTION 1: AUTHORIZATION CHECK ========
-    # if get_jwt_identity() != user_id:
-    #     return {"error": "Not Authorized."}, 401
-    # ======== PROTECTION 1: AUTHORIZATION CHECK ========
-    
     message = Message.query.get(message_id)
     if message == None:
         return {"error": "Not Found"}, 404
